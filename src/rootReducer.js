95528ba7389e172c910069a909974b1b62981c40
gbaer <gbaer@EYI-PC>
2023-10-03 04:57:26 -0400

    first commit

---
+++ src/rootReducer.js
@@ -0,0 +1,22 @@
+const DEFAULT_STATE = {
+  memes: [],
+};
+
+function rootReducer(state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    case "ADD_MEME":
+      return {
+        ...state,
+        memes: [...state.memes, { ...action.meme }],
+      };
+    case "REMOVE_MEME":
+      return {
+        ...state,
+        memes: state.memes.filter((meme) => meme.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
